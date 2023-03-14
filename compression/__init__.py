962a9ff7b698b6d7672fca1bd2f296a7059faa03
eniallator <eniallatormc@gmail.com>
2023-03-14 11:41:34 +0000

    Removed utils import from __init__.py

--- compression/__init__.py
+++ compression/__init__.py
@@ -2,4 +2,3 @@ from .exceptions import InconsistentShape, UnexpectedLeaf
 from .types import Data, DataEntry, CompressedList
 from .compress import compress, serialise
 from .decompress import decompress, deserialise
-from .utils import compress_and_serialise
